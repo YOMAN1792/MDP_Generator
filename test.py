6a504334fbad6f39b41c7357fa3aec9753323ffc
YOMAN1792 <94007829+YOMAN1792@users.noreply.github.com>
2024-08-27 00:47:44 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+name = input("Votre nom : ")
+chiffre = input("Chffre : ")
+blabla = input("djshsdfhk : ")
+
+if name.isalpha() and chiffre.isnumeric() and blabla:
+    print("Ton nom : " + name + ".")
+    print("Ton chiffre : " + chiffre + ".")
+    print("Ton blabla : " + blabla + ".")
+else:
+    print("Fuck You!")
